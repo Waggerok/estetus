5baf21ee649cfb60753424be21968e9aa599bde5
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-07-14 14:51:14 +0300

    created welcome section

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,6 +8,9 @@ import Background from '../components/Background';
 import { IoIosTime, IoMdCart } from 'react-icons/io';
 import { BiSolidDiscount } from 'react-icons/bi';
 
+//Photo
+import Laboratory from '../img/Laboratory.jpg';
+
 const Main = () => {
     return (
         <>
@@ -47,6 +50,32 @@ const Main = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="App">
+                <section className='welcome'>
+                    <div className="welcome__title">
+                        Добро пожаловать в зуботехническую лабораторию Estetus Lab
+                    </div>
+                    <div className="welcome__content">
+                        <div className="welcome__content_photo">
+                            <img src={Laboratory} alt="Laboratory"/>
+                        </div>
+                        <div className="welcome__content_text">
+                            <div className="welcome__content_text-description">
+                                <p>
+                                    Наша компания уже более десяти лет предоставляет свои услуги для различных стоматологических клиник. Мы занимаемся изготовлением высококачественных зубных протезов, коронок, мостов и другие изделий на заказ. 
+                                </p>
+                                <p>
+                                    Наша команда специалистов регулярно совершенствует свои навыки, чтобы идти в ногу со временем, а наши помещения оснащены высококлассным оборудованием.
+                                </p>                               
+                                <p>
+                                    Прямая коммуникация и обмен мнениями между зубными техниками и специалистами нашего фрезцентра послужат основой для успешной реализации ваших требований и пожеланий. Мы гарантируем индивидуальный подход к каждому клиенту, быструю обработку заказов и превосходное качество изделий.
+                                </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </>
     )
