e96fc3633c8ae9cfb5c0e4955c380c597cdc856b
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-07-14 11:15:27 +0300

    navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <Link className='navbar__logo' to='/main'>
+                <img src={logo} alt="logo" />
+                EstetusLab
+            </Link>
+            <div className="navbar__links">
+                <ul>
+                    <li>
+                        <Link className='navbar__links_item' to='/services'>Услуги</Link>
+                    </li>
+                    <li>
+                        <Link className='navbar__links_item' to='/examples'>Фото работ</Link>
+                    </li>
+                    <li>
+                        <Link className='navbar__links_item' to='/contacts'>Контакты</Link>
+                    </li>
+                    <li>
+                        <Link className='navbar__links_item' to='/about'>О компании</Link>                    
+                    </li>        
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
