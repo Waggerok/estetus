e638ae00aba64f18fbed07763c36d31f257ae111
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-07-23 16:18:20 +0300

    created modal

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -17,6 +17,66 @@ const Main = () => {
 
     const [modal, setModal] = useState(false);
     
+    //Logic for modal 
+    const [phone, setPhone] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    
+    const [phoneError, setPhoneError] = useState('Телефон не может быть пустым');
+    const [nameError, setNameError] = useState('Имя не может быть пустым');
+    const [surnameError, setSurnameError] = useState('Фамилия не может быть пустой');
+
+    const validatePhone = (phone) => {
+        const regex = /^(\+7|8)[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+        if (!regex.test(phone)) {
+          setPhoneError('Invalid phone number format');
+          return false;
+        }
+        setPhoneError('');
+        return true;
+      };
+    
+      const validateName = (name) => {
+        const regex = /^[a-zA-Z\s]+$/;
+        if (!regex.test(name.trim())) {
+          setNameError('Invalid name format');
+          return false;
+        }
+        setNameError('');
+        return true;
+      };
+    
+      const validateSurname = (surname) => {
+        const regex = /^[a-zA-Z\s]+$/;
+        if (!regex.test(surname.trim())) {
+          setSurnameError('Invalid surname format');
+          return false;
+        }
+        setSurnameError('');
+        return true;
+      };
+
+      const sendMessage = async () => {
+        if (!validatePhone(phone) || !validateName(name) || !validateSurname(surname)) return;
+    
+        const chatId = '-4108218206'; // Замените на ваш Chat ID
+        const token = '6902058245:AAHfcrwCk-sHFd5GLqAf_xqL2qki3K1KwTE'; // Замените на токен вашего бота
+        const formattedMessage = `Телефон пользователя: ${phone}\nИмя пользователя: ${name}\nФамилия пользоватея: ${surname}`;
+        const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(formattedMessage)}`;
+    
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          setPhone('');
+          setName('');
+          setSurname('');
+          setModal(false)
+        } catch (error) {
+          console.error('Failed to send message:', error);
+        }
+      };
 
     // сделать setModal(false) после того, как заполнил форму
 
@@ -87,34 +147,41 @@ const Main = () => {
                     </div>
                 </section>
 
-
-
-
-
-
                 {/* тут модалка */}
                 <MyModal 
                     visible={modal} 
                     setVisible={setModal}
                 >
                     <h3 style={{textAlign:'center'}}>Заказать консультацию</h3>
-                    <form className='form'>
+                    <form className='form' onSubmit={(e) => { e.preventDefault(); sendMessage(); }}>
                         <div className="form__input">
-                            <input 
+                            
+                            <input
+                            onChange={(e) => setPhone(e.target.value)}
+                            value={phone}
                             type='tel' 
                             className='form__input_tel'
                             placeholder='Ваш телефон:'
                             />
-                            <input 
+
+                            <input                    
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
                             type="text" 
                             className='form__input_name'
                             placeholder='Ваше имя:'
                             />
+                            
                             <input 
+                            onChange={(e) => setSurname(e.target.value)}                           
+                            value={surname}
                             type="text" 
                             className='form__input_name'
                             placeholder='Ваша Фамилия:'
                             />
+                        </div>
+                        <div className='form__agree'>
+                            Нажимая на кнопку "Отправить", вы соглашаетесь на <span>обработку своих персональных данных</span>
                         </div>
                         <div className="form__button">
                             <button type='submit'>Отправить</button>
