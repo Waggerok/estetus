55976a95fbf8f744132b81e477112efffffb115b
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-08-06 15:03:59 +0300

    adding table to price page

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ZrO2 from '../ServicesFolder/Service1';
 import Emax from '../ServicesFolder/Service2';
 import Abatments from '../ServicesFolder/Service3';
@@ -12,9 +12,44 @@ import Thermoplastics from '../ServicesFolder/Service10';
 import Melodient from '../ServicesFolder/Service11';
 
 const Price = () => {
+
+    const [table, setTable] = useState(false);
+
+    const switchTable = () => {
+        if (table === false) {
+            setTable(true)
+        } else {
+            setTable(false);
+        };
+    };
+    
     return (
         <div className='App'>
-           
+            <section className='price'>
+                <div className="price__title">
+                    Цены на наши услуги
+                </div>
+                <div className="price__description">
+                    Здесь вы можете ознакомиться со всеми спецификациями наших изделий, а также узнать цены на интересующие услуги.
+                </div>
+
+                <div className="price__table">
+                    <div className="price__table-name">
+                        Безметалловые коронки ZrO2 (KATANA, PRIME):
+                    </div>
+                    <div className="price__table-button" onClick={switchTable}>
+                        {`>`}
+                    </div>
+                </div>
+                {table
+                ?
+                <div></div>
+                :
+                <div></div>
+                }
+                
+                
+            </section>
         </div>
     )
 };
