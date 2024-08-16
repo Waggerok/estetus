276d31172998f3cca10fa99b3431a9f7173494b4
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-08-16 16:22:46 +0300

    added navbar burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+//Logos
 import logo from '../img/logo.png';
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 const Navbar = () => {
+
+    const [menuIsOpen,setMenuIsOpen] = useState();
+
+    const toggleMenu = () => {
+        setMenuIsOpen(!menuIsOpen);
+    }
+
     return (
-        <div className='navbar'>
-            <Link className='navbar__logo' to='/main'>
-                <img src={logo} alt="logo" />
+        <div className="header">
+            <Link to='/main' className='header__logo'>
+                <img src={logo} alt="logo"/>
                 EstetusLab
             </Link>
-            <div className="navbar__links">
-                <ul>
-                    <li>
-                        <Link className='navbar__links_item' to='/services'>Услуги</Link>
+            <nav className={`header__navbar ${menuIsOpen ? 'active' : ''}`}>
+                <ul className='header__navbar_links'>
+                    <li className="header__navbar_links-item">
+                        <Link to='/services'>Услуги</Link>
                     </li>
-                    <li>
-                        <Link className='navbar__links_item' to='/price'>Цены</Link>
-                    </li>  
-                    <li>
-                        <Link className='navbar__links_item' to='/examples'>Портфолио</Link>
+                    <li className="header__navbar_links-item">
+                        <Link to='/price'>Цены</Link>
                     </li>
-                    <li>
-                        <Link className='navbar__links_item' to='/contacts'>Контакты</Link>
+                    <li className="header__navbar_links-item">
+                        <Link to='/examples'>Портфолио</Link>
+                    </li>
+                    <li className="header__navbar_links-item">
+                        <Link to='/contacts'>Контакты</Link>
                     </li>
                 </ul>
+            </nav>
+            <div className="header__button" onClick={toggleMenu}>
+                <RxHamburgerMenu/>
             </div>
         </div>
     )
