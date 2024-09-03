3c0b282ad55116e3ae7dc57c88c50c09eed639ec
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-09-03 10:42:43 +0300

    full clickable price

--- src/components/MainTable.jsx
+++ src/components/MainTable.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const MainTable = ({table, switchTable, props}) => {
     return (
-        <div className="price__table">
+        <div className="price__table" onClick={switchTable}>
             <div className="price__table-name">
                 {props}
             </div>
-            <div className={table ? 'price__table-button-active' : 'price__table-button'} onClick={switchTable}>
+            <div className={table ? 'price__table-button-active' : 'price__table-button'}>
                 {`>`}
             </div>
         </div>
